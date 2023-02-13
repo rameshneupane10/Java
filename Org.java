79e2985dbe3ab69079dff904d99bffc5f8e425f1
rameshneupane10 <rameshnjr10@gmail.com>
2023-02-13 22:05:06 +0545

    added

---
+++ Org.java
@@ -0,0 +1,13 @@
+public class Org
+{
+    public static void main(String[] args)
+    {
+       
+        System.out.println("Name of the organization : First Assignment Cafe and Bar");
+        System.out.println("\n\nAddress:\n----State: kathmandu\n----Zip Code: 44600\n----Country: Nepal");
+        System.out.println("\n\nContact Information:\n----Mobile Number: 9876543210\n----Email: Firstassign777@gmail.com");
+        
+    }
+
+
+}
