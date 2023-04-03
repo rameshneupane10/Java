197e5dd2db23a9fbd8ea84dd6db3793a591bb395
rameshneupane10 <rameshnjr10@gmail.com>
2023-04-03 06:30:54 +0545

    added

--- Notepad_clone/src/GUI.java
+++ Notepad_clone/src/GUI.java
@@ -1,19 +1,65 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class GUI implements ActionListener
 {
-    JFrame frame = new JFrame();
+    JFrame frame = new JFrame("NOTEPAD");
 
-    JButton button =new JButton();
+    JTextArea textArea = new JTextArea();
+
+    JButton button =new JButton("save");
 
     public void rendor()
     {
-        frame.setVisible(true);
+        frame.setSize(500,500);
+        frame.setResizable(false);
+        frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.add(button);
+        frame.add(textArea);
+        frame.setLocationRelativeTo(null);
+
+        textArea.setBounds(5,0,490,490);
 
 
+        button.setBounds(200,400,90,30);
+        button.addActionListener(this);
 
-        button.addActionListener();
+        File file = new File("text.txt");
+        try
+        {
+            file.createNewFile();
+        }
+        catch (IOException ex)
+        {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        if (e.getSource()==button)
+        {
+            try
+            {
+                FileWriter fileWriter = new FileWriter("text.txt");
+                fileWriter.write(String.valueOf(textArea.getText()));
+                fileWriter.close();
+            }
+            catch (IOException ex)
+            {
+                System.out.println(ex);
+            }
+
+        }
+
     }
 }
+
+
